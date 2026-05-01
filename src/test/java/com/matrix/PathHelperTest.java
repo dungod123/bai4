b22bee45fbf5162899a4b57273cfb90d1df38927
hoang dung <dunghoangvolao123@gmail.com>
2026-05-01 22:43:59 +0700

    suka

--- src/test/java/com/matrix/PathHelperTest.java
+++ src/test/java/com/matrix/PathHelperTest.java
@@ -2,13 +2,20 @@ package com.matrix;
 
 
 import org.junit.jupiter.api.Test;
+import java.io.File;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PathHelperTest {
     @Test
     void testCreateFilePath() {
         PathHelper helper = new PathHelper();
-        // Cố tình bắt nó phải ra chuỗi y hệt Windows nè!
-        assertEquals("docs\\readme.txt", helper.createFilePath("docs", "readme.txt"), "Test này gài bẫy đó nha!");
+        
+        // Lấy dấu phân cách chuẩn của hệ điều hành đang chạy 
+        // (Windows sẽ là '\', còn Linux/macOS sẽ là '/')
+        String expectedOsPath = "docs" + File.separator + "readme.txt";
+        
+        // So sánh đường dẫn chuẩn của OS với kết quả bị hardcode "\"
+        assertEquals(expectedOsPath, helper.createFilePath("docs", "readme.txt"), 
+            "Phen này Linux và macOS tạch chắc luôn nè! 😈");
     }
 }
