e67f75a47bc2dd84ffa48d6f5386c816b01104af
hoang dung <dunghoangvolao123@gmail.com>
2026-05-02 21:11:12 +0700

    uia

--- src/main/java/com/matrix/PathHelper.java
+++ src/main/java/com/matrix/PathHelper.java
@@ -1,9 +1,23 @@
 package com.matrix;
 
+import java.nio.file.Paths;
 
+
+//ERROR:
+
+// public class PathHelper {
+//     // Lỗi chà bá ở đây nè: hardcode dấu \ của Windows! 
+//     public String createFilePath(String folder, String file) {
+//         return folder + "\\" + file; 
+//     }
+// }
+
+
+//FIXED
 public class PathHelper {
-    // Lỗi chà bá ở đây nè: hardcode dấu \ của Windows! (T_T)
     public String createFilePath(String folder, String file) {
-        return folder + "\\" + file; 
+        // Paths.get() sẽ tự động động nối chuỗi bằng '\' trên Windows 
+        // và bằng '/' trên Linux/macOS.
+        return Paths.get(folder, file).toString();
     }
 }
